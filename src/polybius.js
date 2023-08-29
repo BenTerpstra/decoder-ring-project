f31ef869aad3792d222c0ce5ef0871f3f5e0c0fb
Ben Terpstra <bdterpstra@gmail.com>
2023-08-29 14:01:32 -0500

    added modified files for Polybius and Substitution

--- src/polybius.js
+++ src/polybius.js
@@ -5,10 +5,81 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const col1 = ["a", "f", "l", "q", "v"];
+const col2 = ["b", "g", "m", "r", "w"];
+const col3 = ["c", "h", "n", "s", "x"];
+const col4 = ["d", "i/j", "o", "t", "y"];
+const col5 = ["e", "k", "p", "u", "z"];
+const polySquare = [col1, col2, col3, col4, col5];
+
 
   function polybius(input, encode = true) {
     // your solution code here
-  }
+//function polybius (input) {
+        let string = input.toLowerCase();
+
+    if (encode === false) {
+      let noSpaces = [];
+      for (let j=0; j<string.length; j++) {
+        if (string[j] !== " ") {
+          noSpaces.push(string[j]);
+        }
+      //if (!Number.isInteger(noSpaces.length/2)) return false;
+      }
+      if (!Number.isInteger(noSpaces.length/2)) return false;
+      
+          let dePoly = ""
+
+    for (let i=0; i<string.length; i+=0) {
+        if (string[i]===" ") {
+            dePoly += " "
+            i += 1;
+        } else {
+
+        let col = Number(string[i])
+        let row = Number(string[i+1]);
+        let letter = polySquare[col-1][row-1];
+        dePoly += letter;
+        i += 2;
+    }
+    }
+     return dePoly; 
+    }
+    
+    //if (typeof string !== "string") string = string.toString();
+    let poly = "";
+    for (let i=0; i<string.length; i++) {   //iterates across string to select individual characters
+        if (string.charAt(i) === " ") {
+            poly = poly + " ";
+        } else {
+        let char = string.charAt(i);
+//        if (char = " ") {
+//            poly = poly + " ";
+//        } else 
+            if (char === "i" || char === "j") {
+                poly = poly + "42";
+            } else {
+                for (let j=0; j<polySquare.length; j++) {   //iterates through columns of polySquare
+                    if (polySquare[j].includes(char)) {  //selects columns containing desired character
+                    let col = j+1;   
+                        for (let k=0; k<polySquare[j].length; k++) {    //iterates through values w/in columns
+                            if(char === polySquare[j][k]) {     //finds desired character in selected column, converts coordinates to string, then adds to coded message string
+                                let row = k+1;
+                                let coordinates = col + "" + row;
+                                poly = poly + coordinates;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return poly;
+}    
+    
+    
 
   return {
     polybius,
--- src/substitution.js
+++ src/substitution.js
@@ -5,8 +5,66 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  const trueAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 
   function substitution(input, alphabet, encode = true) {
+    if (!alphabet) return false;
+    
+        let string = input.toLowerCase();
+        let key = alphabet;
+              let keyArr = key.split("")
+                  let codeArr = string.split("")
+        if (keyArr.length !== 26) return false;
+    for (let j=0; j<keyArr.length; j++) {
+        for (let k=j+1; k<keyArr.length; k++) {
+            if (keyArr[j] === keyArr[k]) return false;
+        }
+    }
+
+
+    if (encode === false) {
+
+    if (keyArr.length !== 26) return false;
+
+    for (let j=0; j<keyArr.length; j++) {
+        for (let k=j+1; k<keyArr.length; k++) {
+            if (keyArr[j] === keyArr[k]) return false;
+        }
+    }
+    
+
+    for (let i=0; i<keyArr.length; i++) {
+        let char = codeArr[i];
+
+        if (keyArr.includes(char)) {
+            let index = keyArr.indexOf(char)
+            codeArr[i] = trueAlphabet[index];
+        }
+
+    }
+    return codeArr.join(""); 
+    }
+    
+
+
+    
+    //let string = input.toLowerCase();
+    let messageArr = string.split("")
+
+    for (let i=0; i<keyArr.length; i++) {
+        let char = messageArr[i];
+
+        if (trueAlphabet.includes(char)) {
+            let index = trueAlphabet.indexOf(char)
+            messageArr[i] = keyArr[index];
+        }  
+
+    } 
+
+    return messageArr.join("");
+    
+    
     // your solution code here
   }
 
